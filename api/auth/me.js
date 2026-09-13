0a1b129c1e0024817043247e03949188f440a80a
Baroda swim front <barodaswimfront@abcds-MacBook-Pro.local>
2026-09-13 10:46:32 +0530

    Clear old AARK website

--- api/auth/me.js
+++
@@ -1,7 +0,0 @@
-const { verifySessionCookie } = require('../_lib/auth');
-
-module.exports = async (req, res) => {
-  const session = verifySessionCookie(req);
-  if (!session) return res.status(401).json({ authenticated: false });
-  return res.status(200).json({ authenticated: true, email: session.sub });
-};
